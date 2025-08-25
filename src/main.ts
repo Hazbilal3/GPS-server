7c54911b0d7a7631aec4fd44bac429153b63175c
Hazrat Bilal <hazratbilaltech3@gmail.com>
2025-08-25 12:01:37 -0700

    updated

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,9 @@ async function bootstrap() {
     },
   })
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-  await app.listen(port, '0.0.0.0');
+    // await app.listen(process.env.PORT || 3000);
+
+ const port = Number(process.env.PORT ?? '3010');   // default to 3010
+  await app.listen(port, '0.0.0.0');   
 }
 bootstrap();
