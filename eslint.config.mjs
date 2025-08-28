2782e65d9763238efbe286579bb26986683c6286
Hazrat Bilal <hazratbilaltech3@gmail.com>
2025-08-28 10:50:39 -0700

    lint push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default tseslint.config(
   {
     rules: {
       // @ts-ignore
-      '@typescript-eslint/no-explicit-any': 'on',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
     },
